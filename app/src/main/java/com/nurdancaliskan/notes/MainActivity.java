ae2a7421f4586fca64f4eb9f577c840fef6e55a2
nurda <nurdan.caliskan26@hotmail.com>
2022-05-30 14:23:47 +0300

    eleventh commit

--- app/src/main/java/com/nurdancaliskan/notes/MainActivity.java
+++ app/src/main/java/com/nurdancaliskan/notes/MainActivity.java
@@ -3,6 +3,7 @@ package com.nurdancaliskan.notes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +24,8 @@ import com.nurdancaliskan.notes.databinding.ActivityUpdateNotesBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View decorView;
+
     ActivityMainBinding binding;
     FloatingActionButton newNotesBtn;
     NotesViewModel notesViewModel;
@@ -40,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.nurdan.setFrame(120);
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int i) {
+                if (i == 0)
+                    decorView.setSystemUiVisibility(hideSystemBars());
+
+                    }
+        });
+
+
         imageView = findViewById(R.id.nurdan);
         newNotesBtn = findViewById(R.id.newNotesBtn);
         recyclerView = findViewById(R.id.notesRecycler);
@@ -69,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -120,5 +136,24 @@ public class MainActivity extends AppCompatActivity {
             notes3.notesPriority = notePriority;
             notesViewModel.deleteNotes(notes3);
         }
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(hideSystemBars());
+        };
+    }
+
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
     }
 }
